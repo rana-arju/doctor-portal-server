5446bfe7a835021693de08c6c4e4e50233b6bf19
Rana Arju <arjurana530@gmail.com>
2022-05-12 01:06:59 +0600

    get all service data from database

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sptt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('running...');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const appointmentCollections = client.db("doctorPortal").collection("services");
+  //all service get
+  app.get('/services', async(req, res) => {
+    const cursor = appointmentCollections.find({});
+    const services = await cursor.toArray();
+    res.send(services);
+  })
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir)
 
 
 
